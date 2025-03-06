076d07f75acc94c49f267235600c891b130f6712
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-06 12:28:23 -0500

    feat: implement get endpoints

--- src/main/java/com/users/users_backend/controllers/UserController.java
+++ src/main/java/com/users/users_backend/controllers/UserController.java
@@ -1,9 +1,18 @@
 package com.users.users_backend.controllers;
 
+import com.users.users_backend.entities.User;
 import com.users.users_backend.services.IUserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     private final IUserService userService;
@@ -12,5 +21,18 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping
+    public List<User> findAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        Optional<User> user = userService.findById(id);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
 
 }
