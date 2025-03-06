6a24a9eef4fc6abe35836669b0874e7958b59803
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-06 12:47:21 -0500

    feat: implement put, post and delete endpoints

--- src/main/java/com/users/users_backend/controllers/UserController.java
+++ src/main/java/com/users/users_backend/controllers/UserController.java
@@ -2,11 +2,9 @@ package com.users.users_backend.controllers;
 
 import com.users.users_backend.entities.User;
 import com.users.users_backend.services.IUserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,6 +31,37 @@ public class UserController {
             return ResponseEntity.ok(user.get());
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        Optional<User> optionalUser = userService.findById(id);
+        if (optionalUser.isPresent()) {
+            User updatedUser = optionalUser.get();
+            updatedUser.setName(user.getName());
+            updatedUser.setLastName(user.getLastName());
+            updatedUser.setUsername(user.getUsername());
+            updatedUser.setEmail(user.getEmail());
+            updatedUser.setPassword(user.getPassword());
+            return ResponseEntity.status(HttpStatus.OK).body(userService.save(updatedUser));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> delete(@PathVariable Long id) {
+        Optional<User> optionalUser = userService.findById(id);
+        if (optionalUser.isPresent()) {
+            userService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
