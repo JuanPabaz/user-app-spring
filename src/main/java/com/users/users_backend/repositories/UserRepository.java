4b51b1a260f88fe05f87ab681173347371e356ca
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-07 17:55:05 -0500

    feat: add findAll users pageable

--- src/main/java/com/users/users_backend/repositories/UserRepository.java
+++ src/main/java/com/users/users_backend/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.users.users_backend.repositories;
 
 import com.users.users_backend.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-
+    Page<User> findAll(Pageable pageable);
 }
