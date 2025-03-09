7a5fe1daa87b28e7059b6854a3183c6a27fd552a
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-09 16:56:06 -0500

    feat: create re usable variables

--- src/main/java/com/users/users_backend/config/TokenJwtConfig.java
+++ src/main/java/com/users/users_backend/config/TokenJwtConfig.java
@@ -6,5 +6,8 @@ import javax.crypto.SecretKey;
 
 public class TokenJwtConfig {
 
+    public static final String HEADER_AUTHORIZATION = "Authorization";
     public static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String CONTENT_TYPE = "application/json";
 }
