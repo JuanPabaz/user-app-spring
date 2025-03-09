566a05dd0a95583dffa4d486196781f40c38d05d
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-09 16:11:55 -0500

    feat: configure http security rules

--- src/main/java/com/users/users_backend/config/SecurityConfig.java
+++ src/main/java/com/users/users_backend/config/SecurityConfig.java
@@ -13,7 +13,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests.requestMatchers(HttpMethod.GET,"/user/page").permitAll()
+                        authorizeRequests.requestMatchers(HttpMethod.GET,"/user","/user/page").permitAll()
+                                .requestMatchers(HttpMethod.GET,"/user/{id}").hasAnyRole("USER","ADMIN")
+                                .requestMatchers(HttpMethod.POST,"/user").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/user/{id}").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE,"/user/{id}").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
                 .csrf(config -> config.disable())
