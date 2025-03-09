88cef4f0c20c1c02d0f7506132b817bd95b7e413
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-09 16:41:59 -0500

    feat: create TokenJwtConfig with final secret key

---
+++ src/main/java/com/users/users_backend/config/TokenJwtConfig.java
@@ -0,0 +1,10 @@
+package com.users.users_backend.config;
+
+import io.jsonwebtoken.Jwts;
+
+import javax.crypto.SecretKey;
+
+public class TokenJwtConfig {
+
+    public static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();;
+}
