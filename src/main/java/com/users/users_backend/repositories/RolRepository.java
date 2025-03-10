dba65cc54cbe50c72e0f6ac7be00a7fd453eccdc
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-10 16:49:30 -0500

    feat: create role repository and findByName query

---
+++ src/main/java/com/users/users_backend/repositories/RolRepository.java
@@ -0,0 +1,11 @@
+package com.users.users_backend.repositories;
+
+import com.users.users_backend.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RolRepository extends JpaRepository<Role,Integer> {
+
+    Optional<Role> findByName(String name);
+}
